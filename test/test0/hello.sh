c984dc80b441edf284f9444349ca8d86dddeafcb
Jeremiah Orians <jeremiah@pdp10.guru>
2017-07-20 23:15:12 -0400

    Cleanup and reorg of elf headers

--- test/test0/hello.sh
+++ test/test0/hello.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 set -x
-bin/hex2 -f test/test0/elf32.hex2  -f test/test0/mini-libc-mes.hex2 -f test/test0/hello.hex2 --LittleEndian --Architecture 1 --BaseAddress 0x8048000 >| test/results/test0-binary
+bin/hex2 -f elf_headers/elf32.hex2  -f test/test0/mini-libc-mes.hex2 -f test/test0/hello.hex2 --LittleEndian --Architecture 1 --BaseAddress 0x8048000 >| test/results/test0-binary
 chmod +x test/results/test0-binary
 out=$(./test/results/test0-binary 2>&1)
 r=$?
