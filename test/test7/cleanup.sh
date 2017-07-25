b79bf3c513a0cfb6f66b7856ab2fb0925e133449
Jeremiah Orians <jeremiah@pdp10.guru>
2017-07-25 18:27:26 -0400

    Initial commit of test7 AMD64/elf Hex1 example program

---
+++ test/test7/cleanup.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+rm -f test/test7/hold test/test7/proof
+exit 0
---
+++ test/test7/hello.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+set -x
+./bin/M1 -f test/test7/hex1_amd64.M1 --LittleEndian --Architecture 2 >| test/test7/hold
+./bin/hex2 -f elf_headers/elf64.hex2 -f test/test7/hold --LittleEndian --Architecture 2 --BaseAddress 0x00600000 >| test/results/test7-binary
+chmod u+x test/results/test7-binary
+./test/results/test7-binary test/test7/hex1.hex1 > test/test7/proof
+r=$?
+[ $r = 0 ] || exit 1
+out=$(sha256sum -c test/test7/proof.answer)
+[ "$out" = "test/test7/proof: OK" ] || exit 2
+exit 0
