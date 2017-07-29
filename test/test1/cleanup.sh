db999a350a8babe46641c7185436e650c6e3a0e8
Jeremiah Orians <jeremiah@pdp10.guru>
2017-07-29 14:17:16 -0400

    Create a make as a test AMD64/ELF hex1 implemented in Hex0

--- test/test1/cleanup.sh
+++ test/test1/cleanup.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
-rm -f test/test1/hold test/test1/proof
+rm -f test/test1/hold test/test1/proof1 test/test1/proof2
 exit 0
--- test/test1/hello.sh
+++ test/test1/hello.sh
@@ -3,9 +3,15 @@ set -x
 ./bin/M1 -f test/test1/hex.M1 --LittleEndian --Architecture 2 >| test/test1/hold
 ./bin/hex2 -f elf_headers/elf64.hex2 -f test/test1/hold --LittleEndian --Architecture 2 --BaseAddress 0x00600000 >| test/results/test1-binary
 chmod u+x test/results/test1-binary
-./test/results/test1-binary < test/test1/example_test.hex > test/test1/proof
+./test/results/test1-binary < test/test1/hex0.hex0 > test/test1/proof1
 r=$?
 [ $r = 0 ] || exit 1
-out=$(sha256sum -c test/test1/proof.answer)
-[ "$out" = "test/test1/proof: OK" ] || exit 2
+out=$(sha256sum -c test/test1/proof1.answer)
+[ "$out" = "test/test1/proof1: OK" ] || exit 2
+chmod u+x test/test1/proof1
+./test/test1/proof1 < test/test1/hex1.hex0 > test/test1/proof2
+r=$?
+[ $r = 0 ] || exit 3
+out=$(sha256sum -c test/test1/proof2.answer)
+[ "$out" = "test/test1/proof2: OK" ] || exit 4
 exit 0
