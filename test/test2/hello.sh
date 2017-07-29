c4d3fa6a38cf7a05473ce3b5d85c51dff8c9b791
Jeremiah Orians <jeremiah@pdp10.guru>
2017-07-29 16:10:22 -0400

    Minor rename

--- test/test2/hello.sh
+++ test/test2/hello.sh
@@ -2,7 +2,7 @@
 set -x
 ./bin/M1 -f test/test2/hex.M1 --LittleEndian --Architecture 1 -o test/test2/hold
 ./bin/hex2 -f elf_headers/elf32.hex2 -f test/test2/hold --LittleEndian --Architecture 1 --BaseAddress 0x8048000 -o test/results/test2-binary --exec_enable
-./test/results/test2-binary < test/test2/example_test.hex > test/test2/proof
+./test/results/test2-binary < test/test2/hex0.hex0 > test/test2/proof
 r=$?
 [ $r = 0 ] || exit 1
 out=$(sha256sum -c test/test2/proof.answer)
