3102e7ca44ab12e2eb8567763c681123e79a3c35
Jan Nieuwenhuizen <janneke@gnu.org>
2017-12-03 14:35:50 +0100

    build: Update for new Mes libc layout.
    * build.sh: Update for new Mes libc layout.

--- build.sh
+++ build.sh
@@ -16,8 +16,8 @@ $M1 --LittleEndian --Architecture=1\
 # mlibc
 $M1 --LittleEndian --Architecture=1\
     -f $MES_PREFIX/stage0/x86.M1\
-    -f $MES_SEED/libc-mes+tcc.M1\
-    -o libc-mes+tcc.hex2
+    -f $MES_SEED/libc+tcc-mes.M1\
+    -o libc+tcc-mes.hex2
 
 # blood-elf
 $M1 --LittleEndian --Architecture=1\
@@ -28,7 +28,7 @@ $M1 --LittleEndian --Architecture=1\
 $BLOOD_ELF\
     -f $MES_PREFIX/stage0/x86.M1\
     -f $MESCC_TOOLS_SEED/blood-elf.M1\
-    -f $MES_SEED/libc-mes+tcc.M1\
+    -f $MES_SEED/libc+tcc-mes.M1\
     -o blood-elf-blood-elf-footer.M1
 
 $M1 --LittleEndian --Architecture=1\
@@ -38,7 +38,7 @@ $M1 --LittleEndian --Architecture=1\
 $HEX2 --LittleEndian --Architecture=1 --BaseAddress=0x1000000\
        -f $MES_PREFIX/stage0/elf32-header.hex2\
        -f crt1.hex2\
-       -f libc-mes+tcc.hex2\
+       -f libc+tcc-mes.hex2\
        -f blood-elf.hex2\
        -f blood-elf-blood-elf-footer.hex2\
        --exec_enable\
@@ -51,13 +51,13 @@ $M1 --LittleEndian --Architecture=1\
     -o hex2.hex2
 $M1 --LittleEndian --Architecture=1\
     -f $MES_PREFIX/stage0/x86.M1\
-    -f $MES_SEED/libc-mes+tcc.M1\
+    -f $MES_SEED/libc+tcc-mes.M1\
     -o  hex2
 
 $HEX2 --LittleEndian --Architecture=1 --BaseAddress=0x1000000\
       -f $MES_PREFIX/stage0/elf32-header.hex2\
       -f crt1.hex2\
-      -f libc-mes+tcc.hex2\
+      -f libc+tcc-mes.hex2\
       -f hex2.hex2\
       -f $MES_PREFIX/stage0/elf32-footer-single-main.hex2\
        --exec_enable\
@@ -72,7 +72,7 @@ $M1 --LittleEndian --Architecture=1\
 ./blood-elf\
     -f $MES_PREFIX/stage0/x86.M1\
     -f $MESCC_TOOLS_SEED/M1.M1\
-    -f $MES_SEED/libc-mes+tcc.M1\
+    -f $MES_SEED/libc+tcc-mes.M1\
     -o M1-blood-elf-footer.M1
 
 $M1 --LittleEndian --Architecture=1\
@@ -82,7 +82,7 @@ $M1 --LittleEndian --Architecture=1\
 ./hex2 --LittleEndian --Architecture=1 --BaseAddress=0x1000000\
        -f $MES_PREFIX/stage0/elf32-header.hex2\
        -f crt1.hex2\
-       -f libc-mes+tcc.hex2\
+       -f libc+tcc-mes.hex2\
        -f M1.hex2\
        -f M1-blood-elf-footer.hex2\
        --exec_enable\
