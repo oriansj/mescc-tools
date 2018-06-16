61ef872737d9441a6999340030a2f661083f0c47
Jeremiah Orians <jeremiah@pdp10.guru>
2018-06-16 06:49:24 -0400

    Fixed broken test 9

--- test/test9/cleanup.sh
+++ test/test9/cleanup.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-rm -f /test/test9/M1.hex2 /test/test9/footer.M1
+rm -f test/test9/M1.hex2
+rm -f test/test9/footer.M1
 exit 0
--- test/test9/hello.sh
+++ test/test9/hello.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 set -x
 ./bin/blood-elf -f test/test9/M1.M1 -o test/test9/footer.M1
-./bin/M1 --LittleEndian --Architecture=1 -f test/test9/x86.M1 -f test/test9/M1.M1 -f test/test9/footer.M1 -o test/test9/M1.hex2
+./bin/M1 --LittleEndian --Architecture 1 -f test/test9/x86.M1 -f test/test9/M1.M1 -f test/test9/footer.M1 -o test/test9/M1.hex2
 ./bin/hex2 --LittleEndian --Architecture 1 --BaseAddress 0x1000000 -f test/test9/elf32-header.hex2 -f test/test9/crt1.hex2 -f test/test9/libc-mes+tcc.hex2 -f test/test9/M1.hex2 -o test/results/test9-binary --exec_enable
 exit 0
