e8c7fb47398f7fbb2fab0e73d34aff8004fe8c9f
Jeremiah Orians <jeremiah@pdp10.guru>
2018-08-14 21:32:50 -0400

    Harmonize Copyright notices

--- check.sh
+++ check.sh
@@ -1,22 +1,21 @@
 #! /usr/bin/env bash
-# Mes --- Maxwell Equations of Software
 # Copyright © 2017 Jan Nieuwenhuizen <janneke@gnu.org>
 # Copyright © 2017 Jeremiah Orians
 #
-# This file is part of Mes.
+# This file is part of mescc-tools
 #
-# Mes is free software; you can redistribute it and/or modify it
+# mescc-tools is free software; you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or (at
 # your option) any later version.
 #
-# Mes is distributed in the hope that it will be useful, but
+# mescc-tools is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Mes.  If not, see <http://www.gnu.org/licenses/>.
+# along with mescc-tools.  If not, see <http://www.gnu.org/licenses/>.
 
 set -eux
 [ -e bin ] || mkdir -p bin
