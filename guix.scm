6fb4f7e0c4096ea1e4ab88ce60f31ea3c5a0b008
Jeremiah Orians <jeremiah@pdp10.guru>
2018-09-13 14:46:29 -0400

    Broke up guix build scripts and fixed license headers

--- guix.scm
+++ guix.scm
@@ -1,40 +1,36 @@
 ;;; guix.scm -- Guix package definition
-
-;;; Mes --- Maxwell Equations of Software
+;;; mescc-tools
 ;;; Copyright © 2017 Jan Nieuwenhuizen <janneke@gnu.org>
+;;; Copyright 2016 Jeremiah Orians
 
 ;;; Also borrowing code from:
 ;;; guile-sdl2 --- FFI bindings for SDL2
 ;;; Copyright © 2015 David Thompson <davet@gnu.org>
 
 ;;;
-;;; guix.scm: This file is part of Mes.
+;;; guix.scm: This file is part of mescc-tools.
 ;;;
-;;; Mes is free software; you can redistribute it and/or modify it
+;;; mescc-tools is free software; you can redistribute it and/or modify it
 ;;; under the terms of the GNU General Public License as published by
 ;;; the Free Software Foundation; either version 3 of the License, or (at
 ;;; your option) any later version.
 ;;;
-;;; Mes is distributed in the hope that it will be useful, but
+;;; mescc-tools is distributed in the hope that it will be useful, but
 ;;; WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;; GNU General Public License for more details.
 ;;;
 ;;; You should have received a copy of the GNU General Public License
-;;; along with Mes.  If not, see <http://www.gnu.org/licenses/>.
+;;; along with mescc-tools.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;;
 ;; GNU Guix development package.  To build and install, run:
-;;
 ;;   guix package -f guix.scm
 ;;
 ;; To build it, but not install it, run:
-;;
 ;;   guix build -f guix.scm
 ;;
 ;; To use as the basis for a development environment, run:
-;;
 ;;   guix environment -l guix.scm
 ;;
 ;;; Code:
@@ -50,7 +46,6 @@
              (guix build-system gnu)
              (guix gexp)
              (guix git-download)
-             (guix download)
              (guix licenses)
              (guix packages))
 
@@ -73,38 +68,25 @@
          (any (cut string-suffix? <> file) files))
         (_ #f)))))
 
-(define-public mescc-tools
-    (package
-      (name "mescc-tools")
-      (version "0.4")
-      (source (origin
-                (method url-fetch)
-                (uri (string-append "https://github.com/oriansj/mescc-tools/archive/Release_" version ".tar.gz"))
-                (sha256
-                 (base32 "1iwc8xqwzdaqckb4jkkisljrgn8ii4bl7dzk1l2kpv98hsyq9vi1"))))
-      (build-system gnu-build-system)
-      (arguments
-       `(#:make-flags (list (string-append "PREFIX=" (assoc-ref %outputs "out")))
-         #:test-target "test"
-         #:phases
-         (modify-phases %standard-phases
-           (delete 'configure))))
-      (synopsis "tools for the full source bootstrapping process")
-      (description
-       "Mescc-tools is a collection of tools for use in full source bootstrapping process.
-Currently consists of the M0 macro assembler and the hex2 linker.")
-      (home-page "https://github.com/oriansj/mescc-tools")
-      (license gpl3+)))
 
 (define-public mescc-tools.git
- (let ((version "0.4")
-        (revision "0")
-        (commit (read-string (open-pipe "git show HEAD | head -1 | cut -d ' ' -f 2" OPEN_READ))))
-    (package
-      (inherit mescc-tools)
-      (name "mescc-tools.git")
-      (version (string-append version "-" revision "." (string-take commit 7)))
-      (source (local-file %source-dir #:recursive? #t #:select? git-file?)))))
+  (package
+     (name "mescc-tools.git")
+     (build-system gnu-build-system)
+     (arguments
+      `(#:make-flags (list (string-append "PREFIX=" (assoc-ref %outputs "out")))
+        #:test-target "test"
+        #:phases
+        (modify-phases %standard-phases
+          (delete 'configure))))
+     (synopsis "tools for the full source bootstrapping process")
+     (description
+      "Mescc-tools is a collection of tools for use in full source bootstrapping process.
+Currently consists of the M0 macro assembler and the hex2 linker.")
+     (home-page "https://github.com/oriansj/mescc-tools")
+     (license gpl3+)
+     (version (string-append "HEAD-" (string-take (read-string (open-pipe "git show HEAD | head -1 | cut -d ' ' -f 2" OPEN_READ)) 7)))
+     (source (local-file %source-dir #:recursive? #t #:select? git-file?))))
 
 ;; Return it here so `guix build/environment/package' can consume it directly.
 mescc-tools.git
