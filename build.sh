7506213f38f5493e64768adb234a99ac689a2462
Jeremiah Orians <jeremiah@pdp10.guru>
2018-10-06 18:18:36 -0400

    Formally make mescc-tools a M2-Planet build.sh process

--- build.sh
+++ build.sh
@@ -18,8 +18,7 @@
 # along with mescc-tools.  If not, see <http://www.gnu.org/licenses/>.
 
 set -eux
-MES_PREFIX=${MES_PREFIX-../mes}
-MES_SEED=${MES_SEED-../mes-seed}
+M2=${M2-../M2-Planet}
 MESCC_TOOLS_SEED=${MESCC_TOOLS_SEED-../mescc-tools-seed}
 
 #########################################
@@ -31,198 +30,247 @@ MESCC_TOOLS_SEED=${MESCC_TOOLS_SEED-../mescc-tools-seed}
 [ -e bin ] || mkdir -p bin
 
 # blood-elf
-# Create proper debug segment
-$MESCC_TOOLS_SEED/blood-elf\
-    -f $MESCC_TOOLS_SEED/file_print.M1\
-    -f $MESCC_TOOLS_SEED/match.M1\
-    -f $MESCC_TOOLS_SEED/numerate_number.M1\
-    -f $MESCC_TOOLS_SEED/string.M1\
-    -f $MESCC_TOOLS_SEED/blood-elf.M1\
-    -o blood-elf-blood-elf-footer.M1
+$M2/bin/M2-Planet \
+	-f $M2/functions/exit.c \
+	-f $M2/functions/file.c \
+	-f functions/file_print.c \
+	-f $M2/functions/malloc.c \
+	-f $M2/functions/calloc.c \
+	-f functions/match.c \
+	-f blood-elf.c \
+	--debug \
+	-o blood-elf.M1 || exit 1
+
+# Build debug footer
+$MESCC_TOOLS_SEED/blood-elf-0 \
+	-f blood-elf.M1 \
+	-o blood-elf-footer.M1 || exit 2
+
+# Macro assemble with libc written in M1-Macro
+$MESCC_TOOLS_SEED/M1-0 \
+	-f $M2/test/common_x86/x86_defs.M1 \
+	-f $M2/functions/libc-core.M1 \
+	-f blood-elf.M1 \
+	-f blood-elf-footer.M1 \
+	--LittleEndian \
+	--Architecture 1 \
+	-o blood-elf.hex2 || exit 3
+
+# Resolve all linkages
+$MESCC_TOOLS_SEED/hex2-0 \
+	-f elf_headers/elf32-debug.hex2 \
+	-f blood-elf.hex2 \
+	--LittleEndian \
+	--Architecture 1 \
+	--BaseAddress 0x8048000 \
+	-o bin/blood-elf-0 \
+	--exec_enable || exit 4
 
 # Build
 # M1-macro phase
-$MESCC_TOOLS_SEED/M1 --LittleEndian --Architecture 1\
-    -f $MES_PREFIX/lib/x86-mes/x86.M1\
-    -f $MES_SEED/x86-mes/crt1.S\
-    -f $MES_SEED/x86-mes/libc+tcc.S\
-    -f $MESCC_TOOLS_SEED/file_print.M1\
-    -f $MESCC_TOOLS_SEED/match.M1\
-    -f $MESCC_TOOLS_SEED/numerate_number.M1\
-    -f $MESCC_TOOLS_SEED/string.M1\
-    -f $MESCC_TOOLS_SEED/blood-elf.M1\
-    -f blood-elf-blood-elf-footer.M1\
-    -o blood-elf.hex2
-# Hex2-linker phase
-$MESCC_TOOLS_SEED/hex2\
-    --LittleEndian\
-    --Architecture 1\
-    --BaseAddress 0x1000000\
-    -f $MES_PREFIX/lib/x86-mes/elf32-header.hex2\
-    -f blood-elf.hex2\
-    --exec_enable\
-    -o bin/blood-elf
+$M2/bin/M2-Planet \
+	-f $M2/functions/exit.c \
+	-f $M2/functions/file.c \
+	-f functions/file_print.c \
+	-f $M2/functions/malloc.c \
+	-f $M2/functions/calloc.c \
+	-f functions/match.c \
+	-f functions/numerate_number.c \
+	-f functions/string.c \
+	-f M1-macro.c \
+	--debug \
+	-o M1-macro.M1 || exit 5
 
-# M1
-# Create proper debug segment
-./bin/blood-elf \
-    -f $MESCC_TOOLS_SEED/M1.M1\
-    -o M1-footer.M1
+# Build debug footer
+$MESCC_TOOLS_SEED/blood-elf-0 \
+	-f M1-macro.M1 \
+	-o M1-macro-footer.M1 || exit 6
 
-# Build
-# M1-macro phase
-$MESCC_TOOLS_SEED/M1 \
-    --LittleEndian\
-    --Architecture 1\
-    -f $MES_PREFIX/lib/x86-mes/x86.M1\
-    -f $MES_SEED/x86-mes/crt1.S\
-    -f $MES_SEED/x86-mes/libc+tcc.S\
-    -f $MESCC_TOOLS_SEED/file_print.M1\
-    -f $MESCC_TOOLS_SEED/match.M1\
-    -f $MESCC_TOOLS_SEED/numerate_number.M1\
-    -f $MESCC_TOOLS_SEED/string.M1\
-    -f $MESCC_TOOLS_SEED/M1.M1\
-    -f M1-footer.M1\
-    -o M1.hex2
-# Hex2-linker phase
-$MESCC_TOOLS_SEED/hex2 \
-    --LittleEndian\
-    --Architecture 1\
-    --BaseAddress 0x1000000\
-    -f $MES_PREFIX/lib/x86-mes/elf32-header.hex2\
-    -f M1.hex2\
-    --exec_enable\
-    -o bin/M1
+# Macro assemble with libc written in M1-Macro
+$MESCC_TOOLS_SEED/M1-0 \
+	-f $M2/test/common_x86/x86_defs.M1 \
+	-f $M2/functions/libc-core.M1 \
+	-f M1-macro.M1 \
+	-f M1-macro-footer.M1 \
+	--LittleEndian \
+	--Architecture 1 \
+	-o M1-macro.hex2 || exit 7
+
+# Resolve all linkages
+$MESCC_TOOLS_SEED/hex2-0 \
+	-f elf_headers/elf32-debug.hex2 \
+	-f M1-macro.hex2 \
+	--LittleEndian \
+	--Architecture 1 \
+	--BaseAddress 0x8048000 \
+	-o bin/M1-0 \
+	--exec_enable || exit 8
 
 # hex2
-# Create proper debug segment
-./bin/blood-elf\
-    -f $MESCC_TOOLS_SEED/hex2.M1\
-    -o hex2-footer.M1
+$M2/bin/M2-Planet \
+	-f $M2/functions/exit.c \
+	-f $M2/functions/file.c \
+	-f functions/file_print.c \
+	-f $M2/functions/malloc.c \
+	-f $M2/functions/calloc.c \
+	-f functions/match.c \
+	-f functions/numerate_number.c \
+	-f $M2/functions/stat.c \
+	-f hex2_linker.c \
+	--debug \
+	-o hex2_linker.M1 || exit 9
 
-# Build
-# M1-macro phase
-./bin/M1 \
-    --LittleEndian\
-    --Architecture 1\
-    -f $MES_PREFIX/lib/x86-mes/x86.M1\
-    -f $MES_SEED/x86-mes/crt1.S\
-    -f $MES_SEED/x86-mes/libc+tcc.S\
-    -f $MESCC_TOOLS_SEED/file_print.M1\
-    -f $MESCC_TOOLS_SEED/match.M1\
-    -f $MESCC_TOOLS_SEED/numerate_number.M1\
-    -f $MESCC_TOOLS_SEED/string.M1\
-    -f $MESCC_TOOLS_SEED/hex2.M1\
-    -f hex2-footer.M1\
-    -o hex2.hex2
-# Hex2-linker phase
-$MESCC_TOOLS_SEED/hex2 \
-      --LittleEndian\
-      --Architecture 1\
-      --BaseAddress 0x1000000\
-      -f $MES_PREFIX/lib/x86-mes/elf32-header.hex2\
-      -f hex2.hex2\
-      --exec_enable\
-      -o bin/hex2-0
+# Build debug footer
+$MESCC_TOOLS_SEED/blood-elf-0 \
+	-f hex2_linker.M1 \
+	-o hex2_linker-footer.M1 || exit 10
+
+# Macro assemble with libc written in M1-Macro
+$MESCC_TOOLS_SEED/M1-0 \
+	-f $M2/test/common_x86/x86_defs.M1 \
+	-f $M2/functions/libc-core.M1 \
+	-f hex2_linker.M1 \
+	-f hex2_linker-footer.M1 \
+	--LittleEndian \
+	--Architecture 1 \
+	-o hex2_linker.hex2|| exit 11
+
+# Resolve all linkages
+$MESCC_TOOLS_SEED/hex2-0 \
+	-f elf_headers/elf32-debug.hex2 \
+	-f hex2_linker.hex2 \
+	--LittleEndian \
+	--Architecture 1 \
+	--BaseAddress 0x8048000 \
+	-o bin/hex2-0 \
+	--exec_enable || exit 12
+
 
 #########################
 # Phase-1 Self-host     #
 #########################
 
 # blood-elf
-# Create proper debug segment
+# Build debug footer
+./bin/blood-elf-0 \
+	-f blood-elf.M1 \
+	-o blood-elf-footer.M1 || exit 13
+
+# Macro assemble with libc written in M1-Macro
+./bin/M1-0 \
+	-f $M2/test/common_x86/x86_defs.M1 \
+	-f $M2/functions/libc-core.M1 \
+	-f blood-elf.M1 \
+	-f blood-elf-footer.M1 \
+	--LittleEndian \
+	--Architecture 1 \
+	-o blood-elf.hex2 || exit 14
+
+# Resolve all linkages
+./bin/hex2-0 \
+	-f elf_headers/elf32-debug.hex2 \
+	-f blood-elf.hex2 \
+	--LittleEndian \
+	--Architecture 1 \
+	--BaseAddress 0x8048000 \
+	-o bin/blood-elf \
+	--exec_enable || exit 15
+
+# M1-macro
+# Build debug footer
 ./bin/blood-elf \
-    -f $MESCC_TOOLS_SEED/blood-elf.M1\
-    -o blood-elf-blood-elf-footer.M1
+	-f M1-macro.M1 \
+	-o M1-macro-footer.M1 || exit 16
 
-# Build
-# M1-macro phase
-./bin/M1 \
-    --LittleEndian\
-    --Architecture 1\
-    -f $MES_PREFIX/lib/x86-mes/x86.M1\
-    -f $MES_SEED/x86-mes/crt1.S\
-    -f $MES_SEED/x86-mes/libc+tcc.S\
-    -f $MESCC_TOOLS_SEED/file_print.M1\
-    -f $MESCC_TOOLS_SEED/match.M1\
-    -f $MESCC_TOOLS_SEED/numerate_number.M1\
-    -f $MESCC_TOOLS_SEED/string.M1\
-    -f $MESCC_TOOLS_SEED/blood-elf.M1\
-    -f blood-elf-blood-elf-footer.M1\
-    -o blood-elf.hex2
-# Hex2-linker phase
+# Macro assemble with libc written in M1-Macro
+./bin/M1-0 \
+	-f $M2/test/common_x86/x86_defs.M1 \
+	-f $M2/functions/libc-core.M1 \
+	-f M1-macro.M1 \
+	-f M1-macro-footer.M1 \
+	--LittleEndian \
+	--Architecture 1 \
+	-o M1-macro.hex2 || exit 17
+
+# Resolve all linkages
 ./bin/hex2-0 \
-    --LittleEndian\
-    --Architecture 1\
-    --BaseAddress 0x1000000\
-    -f $MES_PREFIX/lib/x86-mes/elf32-header.hex2\
-    -f blood-elf.hex2\
-    --exec_enable\
-    -o blood-elf
-
-# M1
-# Create proper debug segment
-./blood-elf \
-    -f $MESCC_TOOLS_SEED/M1.M1\
-    -o M1-footer.M1
-
-# Build
-# M1-macro phase
-./bin/M1 \
-    --LittleEndian\
-    --Architecture 1\
-    -f $MES_PREFIX/lib/x86-mes/x86.M1\
-    -f $MES_SEED/x86-mes/crt1.S\
-    -f $MES_SEED/x86-mes/libc+tcc.S\
-    -f $MESCC_TOOLS_SEED/file_print.M1\
-    -f $MESCC_TOOLS_SEED/match.M1\
-    -f $MESCC_TOOLS_SEED/numerate_number.M1\
-    -f $MESCC_TOOLS_SEED/string.M1\
-    -f $MESCC_TOOLS_SEED/M1.M1\
-    -f M1-footer.M1\
-    -o M1.hex2
-# Hex2-linker phase
-./bin/hex2-0 \
-    --LittleEndian\
-    --Architecture 1\
-    --BaseAddress 0x1000000\
-    -f $MES_PREFIX/lib/x86-mes/elf32-header.hex2\
-    -f M1.hex2\
-    --exec_enable\
-    -o bin/M1
+	-f elf_headers/elf32-debug.hex2 \
+	-f M1-macro.hex2 \
+	--LittleEndian \
+	--Architecture 1 \
+	--BaseAddress 0x8048000 \
+	-o bin/M1 \
+	--exec_enable || exit 18
 
 # hex2
-# Create proper debug segment
-./bin/blood-elf\
-    -f $MESCC_TOOLS_SEED/hex2.M1\
-    -o hex2-footer.M1
+# Build debug footer
+./bin/blood-elf \
+	-f hex2_linker.M1 \
+	-o hex2_linker-footer.M1 || exit 19
 
-# Build
-# M1-macro phase
+# Macro assemble with libc written in M1-Macro
 ./bin/M1 \
-    --LittleEndian\
-    --Architecture 1\
-    -f $MES_PREFIX/lib/x86-mes/x86.M1\
-    -f $MES_SEED/x86-mes/crt1.S\
-    -f $MES_SEED/x86-mes/libc+tcc.S\
-    -f $MESCC_TOOLS_SEED/file_print.M1\
-    -f $MESCC_TOOLS_SEED/match.M1\
-    -f $MESCC_TOOLS_SEED/numerate_number.M1\
-    -f $MESCC_TOOLS_SEED/string.M1\
-    -f $MESCC_TOOLS_SEED/hex2.M1\
-    -f hex2-footer.M1\
-    -o hex2.hex2
-# Hex2-linker phase
+	-f $M2/test/common_x86/x86_defs.M1 \
+	-f $M2/functions/libc-core.M1 \
+	-f hex2_linker.M1 \
+	-f hex2_linker-footer.M1 \
+	--LittleEndian \
+	--Architecture 1 \
+	-o hex2_linker.hex2|| exit 20
+
+# Resolve all linkages
 ./bin/hex2-0 \
-      --LittleEndian\
-      --Architecture 1\
-      --BaseAddress 0x1000000\
-      -f $MES_PREFIX/lib/x86-mes/elf32-header.hex2\
-      -f hex2.hex2\
-      --exec_enable\
-      -o bin/hex2
+	-f elf_headers/elf32-debug.hex2 \
+	-f hex2_linker.hex2 \
+	--LittleEndian \
+	--Architecture 1 \
+	--BaseAddress 0x8048000 \
+	-o bin/hex2 \
+	--exec_enable || exit 21
+
+# Build pieces that were not needed in bootstrap
+# but are generally useful
+
+# get_machine
+$M2/bin/M2-Planet \
+	-f $M2/functions/exit.c \
+	-f $M2/functions/file.c \
+	-f functions/file_print.c \
+	-f $M2/functions/malloc.c \
+	-f $M2/functions/calloc.c \
+	-f $M2/functions/uname.c \
+	-f get_machine.c \
+	--debug \
+	-o get_machine.M1 || exit 22
+
+# Build debug footer
+./bin/blood-elf \
+	-f get_machine.M1 \
+	-o get_machine-footer.M1 || exit 23
+
+# Macro assemble with libc written in M1-Macro
+./bin/M1 \
+	-f $M2/test/common_x86/x86_defs.M1 \
+	-f $M2/functions/libc-core.M1 \
+	-f get_machine.M1 \
+	-f get_machine-footer.M1 \
+	--LittleEndian \
+	--Architecture 1 \
+	-o get_machine.hex2 || exit 24
+
+# Resolve all linkages
+./bin/hex2 \
+	-f elf_headers/elf32-debug.hex2 \
+	-f get_machine.hex2 \
+	--LittleEndian \
+	--Architecture 1 \
+	--BaseAddress 0x8048000 \
+	-o bin/get_machine \
+	--exec_enable || exit 25
+
+# Clean up after ourself
+rm -f bin/blood-elf-0 bin/M1-0 bin/hex2-0
 
 # TODO
-touch bin/exec_enable
-touch bin/get_machine
-touch bin/kaem
+# exec_enable
+# kaem
