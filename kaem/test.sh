affdf93581b9064de23220a735406ce82e990b61
fosslinux <fosslinux@aussies.space>
2020-02-27 20:19:31 +1100

    Add two more tests

--- kaem/test.sh
+++ kaem/test.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
+
 pwd > kaem/test/test07/answer # Generate test07 answer
-for i in $(seq 13) ; do
+
+for i in $(seq 15) ; do
     TEST=$(printf "%02d" $i)
     bin/kaem -f kaem/test/test${TEST}/kaem.test > kaem/test/test${TEST}/output 2>&1
     if cmp kaem/test/test${TEST}/answer kaem/test/test${TEST}/output > /dev/null 2>&1 ; then
