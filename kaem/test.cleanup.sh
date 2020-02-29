7724a05fb9dd29327e18e99b8a5a90a37983ae62
fosslinux <fosslinux@aussies.space>
2020-03-01 07:25:39 +1100

    Restructure test system to standard test system

--- kaem/test.cleanup.sh
+++ kaem/test.cleanup.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-rm kaem/test/test*/output
-rm kaem/test/test07/answer
+rm kaem/test/results/*
--- kaem/test.sh
+++ kaem/test.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-pwd > kaem/test/test07/answer # Generate test07 answer
-
-for i in $(seq 15) ; do
+for i in $(seq 0 15) ; do
     TEST=$(printf "%02d" $i)
-    bin/kaem -f kaem/test/test${TEST}/kaem.test > kaem/test/test${TEST}/output 2>&1
-    if cmp kaem/test/test${TEST}/answer kaem/test/test${TEST}/output > /dev/null 2>&1 ; then
-        printf "%s: PASSED\\n" "${TEST}"
-    else
-        printf "%s: FAILED\\n" "${TEST}"
-    fi
+    bin/kaem -f kaem/test/test${TEST}/kaem.test > kaem/test/results/test${TEST}-output 2>&1
 done
+sha256sum -c kaem/test/test.answers  
